e4c9ec74c71f1203e84c7f8592e0e57b7568d981
LuizEddie <contato.luiz1997@gmail.com>
2022-12-11 20:38:00 -0300

    Aula 350

---
+++ valida_login.php
@@ -0,0 +1,20 @@
+<?php
+    /*
+    print_r($_GET);
+    echo '<br>';
+
+    echo $_GET['email'];
+    echo '<br>';
+    echo $_GET['senha'];
+    */
+    
+    print_r($_POST);
+
+    echo '<br>';
+
+    echo $_POST['email'];
+    echo '<br>';
+    echo $_POST['senha'];
+
+
+?>
